ad6400c5f996edfaaa2c39ecc717ccfa51183d66
Ben Clough <benkclough@gmail.com>
2020-03-05 14:27:56 -0700

    Ready to try to train?

--- data/dataloader.py
+++ data/dataloader.py
@@ -3,11 +3,14 @@ import pandas as pd
 import numpy as np
 
 class SudokuDataset(Dataset):
-    def __init__(self, file, mono=True):
+    def __init__(self, file, mono=True, cap_train=None):
         self.data = pd.read_csv(file)
         self.mono = mono
+        self.cap_train = cap_train
         
     def __len__(self):
+        if self.cap_train is not None:
+            return min(len(self.data), cap_train)
         return len(self.data)
        
     def __getitem__(self, i):
@@ -43,12 +46,12 @@ class SudokuDataset(Dataset):
         res = res.reshape((9,9,9))
         return res
     
-def get_loader(root, train=True, mono=True, batch_size=42):
+def get_loader(root, train=True, mono=True, batch_size=42, cap_train=None):
     if train:
         source = root + 'sudoku.csv'
     else:
         source = root + 'sudoku_test.csv'
         
-    dataset = SudokuDataset(source, mono)
+    dataset = SudokuDataset(source, mono, cap_train)
     return DataLoader(dataset, shuffle=train, batch_size=batch_size)
         
