cb9a07c1f5e78c053b130f600cdb5691282294a8
Kenneth Christensen <1kennethac@gmail.com>
2020-03-07 09:01:51 -0700

    Make GPU optional and all aync for stacked_to_mono.

--- data/model_util.py
+++ data/model_util.py
@@ -5,7 +5,19 @@ def count_params(model):
     model_parameters = filter(lambda p: p.requires_grad, model.parameters())
     params = sum([np.prod(p.size()) for p in model_parameters])
 
-def stacked_to_mono(puzzles):
-    zeros_mask = torch.all(puzzles == 0, dim=1).cuda()
-    maxes = puzzles.argmax(dim=1).cuda()
-    return torch.where(zeros_mask, torch.zeros(zeros_mask.size()).cuda(), maxes.float().cuda() + 1).cuda()
+def stacked_to_mono(puzzles, cuda=True):
+    zeros_mask = torch.all(puzzles == 0, dim=1)
+    maxes = puzzles.argmax(dim=1).float() + 1
+    zeros = torch.zeros(zeros_mask.size())
+    
+    if cuda:
+        zeros_mask = zeros_mask.cuda(async=False)
+        maxes = maxes.cuda(async=False)
+        zeros = zeros.cuda(async=False)
+
+    result = torch.where(zeros_mask, zeros, maxes)
+
+    if cuda:
+        result = result.cuda(async=False)
+
+    return result
